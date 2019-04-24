78eae856d8a7344fa5bac480a0cb5036b7d67f59
Karl Huang <karl.huang2009@gmail.com>
2019-04-23 18:54:11 -0700

    pls merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,9 @@ import {
   XPosition,
   YPosition,
 } from '@dailybruin/lux'
-<<<<<<< HEAD
 import GifPhoto from '../components/gif'
-=======
 import { DUMMY_DO_NOT_USE_THIS_OR_YOU_WILL_BE_FIRED_Navbar } from '../components/Navbar'
 
->>>>>>> master
 export const query = graphql`
   query {
     site {
@@ -39,7 +36,6 @@ export const query = graphql`
 `
 const IndexPage = ({ data }) => (
   <>
-<<<<<<< HEAD
     <Head {...data.site.siteMetadata} />
     {/* <CoverPhoto
       headline={data.kerckhoffArticle.headline}
@@ -49,11 +45,6 @@ const IndexPage = ({ data }) => (
       yPosition={YPosition.Center}
     /> */}
     <GifPhoto
-=======
-    {/* <Head {...data.site.siteMetadata} /> */}
-    <DUMMY_DO_NOT_USE_THIS_OR_YOU_WILL_BE_FIRED_Navbar />
-    <CoverPhoto
->>>>>>> master
       headline={data.kerckhoffArticle.headline}
       authors={data.kerckhoffArticle.author}
       imageURL="https://chancellor.ucla.edu/wp-content/uploads/2018/07/ChancellorBlock_1366x912_acf_cropped.jpg"
